b6ab070079c3fa8b6e3f281de327a771ed810e85
Marla Glasser <mbglasser@gmail.com>
2018-04-19 12:08:27 -0400

    add login feature spec. Fix user factory typo.

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
 	factory :user do
 		sequence(:name) { |n| "test#{n}" }
-		sequense(:email) { |n| "test#{n}@test.com" }
+		sequence(:email) { |n| "test#{n}@test.com" }
 		password '123456'
 		password_confirmation '123456'
 	end
