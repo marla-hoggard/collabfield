30ab3a97eababdf4b2c47df2b970afffce3c0af7
Marla Glasser <mbglasser@gmail.com>
2018-04-19 11:52:14 -0400

    Specs for NavHelper collapsible_links

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,14 +1,5 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
-require 'capybara/poltergeist'
-require 'factory_girl_rails'
-require 'capybara/rspec'
-
-config.include Devise::Test::IntegrationHelpers, type: :feature
-config.include FactoryGirl::Syntax::Methods
-Capybara.javascript_driver = :poltergeist
-Capybara.server = :puma 
-
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 # Prevent database truncation if the environment is production
@@ -83,4 +74,13 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+
+  require 'capybara/poltergeist'
+  require 'factory_bot_rails'
+  require 'capybara/rspec'
+
+  config.include Devise::Test::IntegrationHelpers, type: :feature
+  config.include FactoryBot::Syntax::Methods
+  Capybara.javascript_driver = :poltergeist
+  Capybara.server = :puma 
 end
